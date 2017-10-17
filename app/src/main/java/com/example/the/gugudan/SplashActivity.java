9fdda61acc0ec7aaa775967f804d4cf4d6042635
HanSangYeol <samh2006@naver.com>
2017-10-17 10:44:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/the/gugudan/SplashActivity.java
@@ -0,0 +1,50 @@
+package com.example.the.gugudan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SplashActivity extends BaseActivity {
+
+    private android.widget.ImageView iconImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splish);
+        bindView();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(mContext, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 2000);
+
+        Glide.with(mContext).load(R.drawable.gugudan_icon_1).into(iconImg);
+
+    }
+
+    @Override
+    public void bindView() {
+        this.iconImg = (ImageView) findViewById(R.id.iconImg);
+    }
+}
